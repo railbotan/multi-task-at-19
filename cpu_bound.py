33bb730ada7fadb3cd6fb16861d39b9c579712ac
ArtyomMoiseev <moiseev.artlenalex@gmail.com>
2021-11-27 15:41:25 +0500

    Добавил cpu_bound.py

---
+++ cpu_bound.py
@@ -0,0 +1,24 @@
+from hashlib import md5
+from random import choice
+import datetime
+from concurrent.futures import ProcessPoolExecutor
+print(datetime.datetime.now())
+
+def gen_print(z):
+    while True:
+        s = "".join([choice("0123456789") for i in range(50)])
+        h = md5(s.encode('utf8')).hexdigest()
+        if h.endswith("00000"):
+            print(s, h)
+            print(datetime.datetime.now())
+
+
+def main():
+    with ProcessPoolExecutor(max_workers=61) as executor:
+        for c in executor.map(gen_print, range(100)):
+            pass
+
+
+if __name__ == '__main__':
+    main()
+
--- wiki_checker.py
+++ wiki_checker.py
@@ -5,14 +5,13 @@ from concurrent.futures import ThreadPoolExecutor
 print(datetime.datetime.now())
 links = open('wiki_urls.txt', encoding='utf8').read().split('\n')
 
-
 def try_open(url):
     try:
         request = Request(
             url,
             headers={'User-Agent': 'Mozilla/5.0 (Windows NT 9.0; Win65; x64; rv:97.0) Gecko/20105107 Firefox/92.0'},
         )
-        resp = urlopen(request, timeout=5, cafile=certifi.where())
+        resp = urlopen(request, timeout=100, cafile=certifi.where())
         code = resp.code
         resp.close
         return code
