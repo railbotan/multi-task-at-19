eeee7423b71718e37fc678c2f55edf6d43b73dcf
frog016 <mixalyov.nikita@mail.ru>
2021-11-28 21:40:00 +0500

    Async navigation by links (use ThreadPoolExecutor)

--- io-bound_requests.py
+++ io-bound_requests.py
@@ -1,16 +1,23 @@
-from urllib.request import Request, urlopen
+import urllib
+from urllib.request import Request
+import concurrent.futures
+
+
+def load_url(url, timeout):
+    with urllib.request.urlopen(url, timeout=timeout) as conn:
+        return conn.code
+
 
 links = open('res.txt', encoding='utf8').read().split('\n')
 
-for url in links:
-    try:
-        request = Request(
-            url,
-            headers={'User-Agent': 'Mozilla/5.0 (Windows NT 9.0; Win65; x64; rv:97.0) Gecko/20105107 Firefox/92.0'},
-        )
-        resp = urlopen(request, timeout=5)
-        code = resp.code
-        print(code)
-        resp.close()
-    except Exception as e:
-        print(url, e)
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    future_to_url = {executor.submit(load_url, url, 5): url for url in links}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            code = future.result()
+        except Exception as exception:
+            print('%r generated an exception: %s' % (url, exception))
+        else:
+            print(f'{code:d} code')
