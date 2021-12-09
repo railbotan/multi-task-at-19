de26377699131dd070e6f4acdb58b15d019bc961
HappyRoma <krivonogov.roma@mail.ru>
2021-12-09 23:29:40 +0500

    io-bound task

---
+++ iobound.py
@@ -0,0 +1,19 @@
+from urllib.request import Request, urlopen
+from urllib.parse import unquote
+import concurrent.futures
+
+links = open('res.txt', encoding='utf8').read().split('\n')
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    for url in links:
+        try:
+            request = Request(
+                url,
+                headers={'User-Agent': 'Mozilla/5.0 (Windows NT 9.0; Win65; x64; rv:97.0) Gecko/20105107 Firefox/92.0'},
+            )
+            resp = urlopen(request, timeout=5)
+            code = resp.code
+            print(code)
+            resp.close()
+        except Exception as e:
+            print(url, e)
