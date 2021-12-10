06580471dc42e3622d458aa0d3cfd706aa741a98
HappyRoma <krivonogov.roma@mail.ru>
2021-12-10 20:21:40 +0500

    reworked tasks

--- CPUBound.py
+++ CPUBound.py
@@ -2,15 +2,20 @@ from hashlib import md5
 from random import choice
 import concurrent.futures
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
-    x = 0
+
+def generator(n):
     while True:
         s = "".join([choice("0123456789") for i in range(50)])
         h = md5(s.encode('utf8')).hexdigest()
-
         if h.endswith("00000"):
-            print(s, h)
-            x += 1
+            return s+","+h
 
-        if x == 3:
-            break
+
+def main():
+    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
+        for coin in zip(executor.map(generator, range(3))):
+            print(coin)
+
+
+if __name__ == '__main__':
+    main()
